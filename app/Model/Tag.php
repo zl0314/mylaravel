c903ffb80662825e426b04073749978a4ab3413a
Zhanglong <nice5good@126.com>
2018-02-02 19:38:05 +0800

    标签优化

--- app/Model/Tag.php
+++ app/Model/Tag.php
@@ -7,4 +7,9 @@ use Illuminate\Database\Eloquent\Model;
 class Tag extends Model
 {
     protected $guarded = [];
+
+    public function articles ()
+    {
+        return $this->morphedByMany( 'App\Model\Article', 'tagable' );
+    }
 }
